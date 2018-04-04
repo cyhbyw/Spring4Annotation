0f1e6103db3d85f77701d277497f4b4c0876f4ae
cyhbyw <swust2009cyh@qq.com>
2018-04-04 14:59:04 +0800

    initial import (annotation Spring MVC)

---
+++ 03-springmvc-annotation/src/main/java/com/atguigu/config/AppConfig.java
@@ -0,0 +1,41 @@
+package com.atguigu.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScan.Filter;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import com.atguigu.controller.MyFirstInterceptor;
+
+//SpringMVC只扫描Controller；子容器
+//useDefaultFilters=false 禁用默认的过滤规则；
+@ComponentScan(value = "com.atguigu", includeFilters = { @Filter(type = FilterType.ANNOTATION, classes = { Controller.class }) }, useDefaultFilters = false)
+@EnableWebMvc
+public class AppConfig extends WebMvcConfigurerAdapter {
+
+    //定制
+
+    //视图解析器
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        registry.jsp("/WEB-INF/views/", ".jsp");
+    }
+
+    //静态资源访问
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+
+    //拦截器
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new MyFirstInterceptor()).addPathPatterns("/**");
+    }
+
+}
---
+++ 03-springmvc-annotation/src/main/webapp/index.jsp
@@ -0,0 +1,21 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <title>Home</title>
+</head>
+<body>
+<img alt="" src="upload_3.jpg">
+<br/>
+<a href="/hello">Hello</a>
+<br/>
+<a href="/suc">Success</a>
+<br/>
+<a href="/async01">Async</a>
+<br/>
+<a href="/createOrder">CreateOrder</a>
+<br/>
+<a href="/create">Create</a>
+</body>
+</html>
